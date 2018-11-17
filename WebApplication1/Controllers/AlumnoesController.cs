75c1222b6f22f3eb0341b26f4b619ab41f091764
Yoryo Fuentes <jorge.iv.fu@gmail.com>
2018-11-17 19:25:21 -0300

    Se creo la vista y cosas respecto de la tabla alimno

---
+++ WebApplication1/Controllers/AlumnoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AlumnoesController : Controller
+    {
+        private ClassiEntities db = new ClassiEntities();
+
+        // GET: Alumnoes
+        public ActionResult Index()
+        {
+            return View(db.Alumnos.ToList());
+        }
+
+        // GET: Alumnoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alumno alumno = db.Alumnos.Find(id);
+            if (alumno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumno);
+        }
+
+        // GET: Alumnoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alumnoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDalumno,Nombre,Apellido")] Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Alumnos.Add(alumno);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(alumno);
+        }
+
+        // GET: Alumnoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alumno alumno = db.Alumnos.Find(id);
+            if (alumno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumno);
+        }
+
+        // POST: Alumnoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDalumno,Nombre,Apellido")] Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(alumno).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(alumno);
+        }
+
+        // GET: Alumnoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alumno alumno = db.Alumnos.Find(id);
+            if (alumno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumno);
+        }
+
+        // POST: Alumnoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Alumno alumno = db.Alumnos.Find(id);
+            db.Alumnos.Remove(alumno);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
